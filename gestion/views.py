4f06ff4312596b5309a7be85c89121d0b554eb7f
SamuelPasquier <samuel.pasquier@ucb.edu.bo>
2023-12-13 18:24:20 -0400

    Update gestion materias

--- gestion/views.py
+++ gestion/views.py
@@ -15,83 +15,27 @@ class AdminOnlyView(LoginRequiredMixin, View):
 
     def get(self, request):
         if request.user.is_staff:
-            permisos = RegistroPermisos.objects.filter(estado='Pendiente')
-            ids = []
+            permisos = RegistroPermisos.objects.filter(estado='Pendiente').select_related('project')
+
+            # Organizar permisos en un diccionario
+            permisos_dict = {}
             for permiso in permisos:
-                ids.append(permiso.id_solicitud)
+                if permiso.id_solicitud not in permisos_dict:
+                    permisos_dict[permiso.id_solicitud] = {'permiso': permiso,'estudiante': permiso.project, 'materias': [],}
+                permisos_dict[permiso.id_solicitud]['materias'].append({
+                    'materia': permiso.materia,
+                    'horaInicio': permiso.horaInicio,
+                    'horaFin': permiso.horaFin,
+                    'fecha': permiso.fecha,
+                })
 
-            result_list = []
-            index_list = []
+            # Convertir el diccionario a una lista para usar en la plantilla
+            permisos_list = list(permisos_dict.values())
 
-            count_dict = {}
-
-            for index, item in enumerate(ids):
-                if item in count_dict:
-                    count_dict[item][0] += 1
-                    count_dict[item][1].append(index)
-                else:
-                    count_dict[item] = [1,[index]]
-
-            for item, (count,indices) in count_dict.items():
-                if count > 1:
-                    result_list.extend([(item,) * count])
-                    if len(indices) > 1:
-                        index_list.append(tuple(indices))
-
-            combined_list = []
-            for i, permiso in enumerate(permisos):
-                materias = []
-                materias = [(permiso.materia,permiso.horaInicio,permiso.horaFin,permiso.fecha)]
-                for ind,j in enumerate(index_list):
-                    if i == j[0]:
-                        auxPermisos = RegistroPermisos.objects.filter(id_solicitud = result_list[ind][0],estado='Pendiente')
-                        
-                        materias = []
-                        for auxPermiso in auxPermisos:
-                            materias.append((auxPermiso.materia,auxPermiso.horaInicio,auxPermiso.horaFin,auxPermiso.fecha))
-
-                combined_list.append((permiso, permiso.project, materias))
-
-            sorted_data = sorted(index_list, key=lambda x: x[1],reverse=True)
-            
-            if len(sorted_data)!=0:
-                for i in sorted_data:
-                    del combined_list[i[1]]
-
-            return render(request, 'petitions/petition_list.html', {'permisos': combined_list})
+            return render(request, 'petitions/petition_list.html', {'permisos': permisos_list})
         else:
             return render(request, 'petitions/not_authorized.html')
-    
-    def post(self, request):
-        if request.POST.get('_method') == 'put':
-            petition_id = request.POST.get('petition_id')
-            observation = request.POST.get('observation')
 
-            # Retrieve the object to update using the petition_id
-            obj_to_update = get_object_or_404(RegistroPermisos, id=petition_id)
-
-            # Update the observation field
-            obj_to_update.observaciones = observation
-
-            # Save the changes to the object
-            obj_to_update.save()
-
-            # Return a JSON response to indicate success
-            return JsonResponse({'message': 'Observation updated successfully'})
-        else:
-            permiso_id = request.POST.get('permisoid_solicitud')
-            new_status = request.POST.get('status')
-            permisos = RegistroPermisos.objects.filter(id_solicitud=permiso_id)
-            if new_status == 'Aceptado' or new_status == 'Rechazado' or new_status == 'Observado':
-                for i in permisos:
-                    form = RegistroPermisosForm(request.POST, instance=i)
-                for i in permisos:
-                    i.estado = new_status
-                    i.save()
-    
-                return redirect('petition_list')
-            else:
-                return redirect('petition_list')
 
 class UpdateObservationView(View):
     def post(self, request):
